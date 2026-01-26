524920d7afd455e05ca882b16f5b7e21d2ff4f71
Honor Pride <125513039+CHARAF-EDDAHHANI@users.noreply.github.com>
2026-01-26 21:19:11 +0100

    Clean up server.js by removing unused code
    Removed commented-out code for serving static files and catch-all route.

--- Server/server.js
+++ Server/server.js
@@ -30,14 +30,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
-// Serve static files from the React app in production
-//if (process.env.NODE_ENV === 'production') {
- // app.use(express.static(path.join(__dirname, '../../client/dist')));
-
-  // Catch-all route to serve the React app
-  //app.get('*', (req, res) => {
-    //res.sendFile(path.resolve(__dirname, '../../client/dist/index.html'));
-  //});
-//}
